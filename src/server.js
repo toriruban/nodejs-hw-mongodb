67d431e8aae617a36be5dc5d59bccb1d5d06f4c2
ToriRuban <toriruban@icloud.com>
2025-08-08 00:57:30 +0300

    feat: add GET /contacts/:contactId with service, 404 handling, and id validation

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { getAllContacts } from './services/contacts';
+import { getAllContacts, getContactById } from './services/contacts';
 
 export const setupServer = () => {
     const app = express();
@@ -29,4 +29,20 @@ export const setupServer = () => {
             data: contacts,
           });
       });  
+
+      app.get('/contacts/:contactId', async(req, res, next) => {
+        const { contactId } = req.params;
+        const contact = await getContactById(contactId);
+        if(!contact) {
+          res.status(404).json({
+            message:'Contact not found',
+          });
+          return;
+        }
+        res.status(200).json({
+          status: 200,
+	        message: `Successfully found contact with id ${contactId}!`,
+	        data: contact,
+        });
+      });
 }
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -4,3 +4,8 @@ export const getAllContacts = async () => {
     const contacts = await ContactsCollection.find();
     return contacts;
 }
+
+export const getContactById = async (contactId) => {
+    const contact = await ContactsCollection.findById(contactId);
+    return contact;
+}
