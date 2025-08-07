76ee074a3fe428228fa37709bd2b7d329c1603a8
ToriRuban <toriruban@icloud.com>
2025-08-08 00:14:29 +0300

    feat: connect project to MongoDB Atlas and display data in Compass

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import { initMongoConnection } from './db/initMongoConnection';
 dotenv.config();
 
 const PORT = Number(process.env.PORT) || 3000;
-setupServer(PORT);
+
 
 const bootstrap = async () => {
     await initMongoConnection();
-    startServer();
+    setupServer(PORT);
 };
 bootstrap();
 
