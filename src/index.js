a618da5b51cbeaad95e74b5f12fd8886d711130a
ToriRuban <toriruban@icloud.com>
2025-08-07 20:08:01 +0300

    feat: create cluster in MongoDB Atlas and connect to MongoDB

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import dotenv from 'dotenv';
 import { setupServer } from './server.js';
+import { initMongoConnection } from './db/initMongoConnection';
 
 dotenv.config();
+
 const PORT = Number(process.env.PORT) || 3000;
 setupServer(PORT);
+
+const bootstrap = async () => {
+    await initMongoConnection();
+    startServer();
+};
+bootstrap();
+
+
+
 
