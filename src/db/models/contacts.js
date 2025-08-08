05d6fd946785f49bbc204602deadf487448696a4
ToriRuban <toriruban@icloud.com>
2025-08-08 17:14:40 +0300

    fix: contactsSchema to write createdAt, updatedAt in readable format

--- src/db/models/contacts.js
+++ src/db/models/contacts.js
@@ -30,4 +30,16 @@ const contactsSchema = new Schema(
     },
 )
 
+contactsSchema.set('toJSON', {
+    transform: (doc, ret) => {
+      if (ret.createdAt) {
+        ret.createdAt = new Date(ret.createdAt).toLocaleString('uk-UA');
+      }
+      if (ret.updatedAt) {
+        ret.updatedAt = new Date(ret.updatedAt).toLocaleString('uk-UA');
+      }
+      return ret;
+    }
+  });
+
 export const ContactsCollection = model('contacts', contactsSchema);
