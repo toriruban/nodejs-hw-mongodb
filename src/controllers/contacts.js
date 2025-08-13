27985c70a311adb9dcd25177012e9e0ef6c38c9b
ToriRuban <toriruban@icloud.com>
2025-08-13 19:12:46 +0300

    refactor(contacts): move controllers to src/controllers/contacts.js

---
+++ src/controllers/contacts.js
@@ -0,0 +1,31 @@
+import { getAllContacts, getContactById } from '../services/contacts.js';
+
+export const getContactsController = async(req, res) => {
+    try{
+        const contacts = await getAllContacts();
+        res.status(200).json({
+          status: 200,
+          message: "Successfully found contacts!",
+          data: contacts,
+        });
+    } catch (err) {
+        next(err);
+    } 
+};
+
+export const getContactsByIdController = async(req, res) => {
+    try{
+        const { contactId } = req.params;
+        const contact = await getContactById(contactId);
+        if(!contact) {
+          return res.status(404).json({ message:'Contact not found'});
+        }
+        res.status(200).json({
+          status: 200,
+          message: `Successfully found contact with id ${contactId}!`,
+          data: contact,
+        });
+      } catch (err) {
+        next(err);
+      }
+}
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,33 +1,7 @@
 import { Router } from 'express';
-import { getAllContacts, getContactById } from '../services/contacts.js';
+import { getContactsController, getContactsByIdController } from '../controllers/contacts.js';
 
 const router = Router();
-    router.get('/', async (req, res, next) => {
-        try{
-          const contacts = await getAllContacts();
-          res.status(200).json({
-            status: 200,
-            message: "Successfully found contacts!",
-            data: contacts,
-          });
-        } catch (err) {
-          next(err);
-        } 
-      });  
-    router.get('/:contactId', async(req, res, next) => {
-        try{
-          const { contactId } = req.params;
-          const contact = await getContactById(contactId);
-          if(!contact) {
-            return res.status(404).json({ message:'Contact not found'});
-          }
-          res.status(200).json({
-            status: 200,
-            message: `Successfully found contact with id ${contactId}!`,
-            data: contact,
-          });
-        } catch (err) {
-          next(err);
-        }
-      });
+    router.get('/',getContactsController);  
+    router.get('/:contactId', getContactsByIdController);
 export default router;
