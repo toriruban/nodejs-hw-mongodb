f9c3b98330dcc4fd9510b0c0efb30ac25662ba94
ToriRuban <toriruban@icloud.com>
2025-08-13 19:28:41 +0300

    feat: ctrlWrapper for controllers

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,7 +1,8 @@
 import { Router } from 'express';
 import { getContactsController, getContactsByIdController } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
-    router.get('/',getContactsController);  
-    router.get('/:contactId', getContactsByIdController);
+    router.get('/',ctrlWrapper(getContactsController));  
+    router.get('/:contactId', ctrlWrapper(getContactsByIdController));
 export default router;
