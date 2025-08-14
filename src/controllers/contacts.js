2a29a81f46e7976d67e49b96ac006c8ec6a5fdd4
ToriRuban <toriruban@icloud.com>
2025-08-14 22:12:28 +0300

    fix(errors): controllers/contacts.js

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -2,20 +2,14 @@ import createHttpError from 'http-errors';
 import { getAllContacts, getContactById } from '../services/contacts.js';
 
 export const getContactsController = async(req, res) => {
-    try{
         const contacts = await getAllContacts();
         res.status(200).json({
           status: 200,
           message: "Successfully found contacts!",
           data: contacts,
         });
-    } catch (err) {
-        next(err);
-    } 
-};
-
+    };
 export const getContactsByIdController = async(req, res) => {
-    try{
         const { contactId } = req.params;
         const contact = await getContactById(contactId);
         if(!contact) {
@@ -26,7 +20,4 @@ export const getContactsByIdController = async(req, res) => {
           message: `Successfully found contact with id ${contactId}!`,
           data: contact,
         });
-      } catch (err) {
-        next(err);
-      }
 }
