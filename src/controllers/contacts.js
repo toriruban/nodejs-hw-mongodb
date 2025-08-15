d67e662942e22f0517e3a802d957168dbabd4cb2
ToriRuban <toriruban@icloud.com>
2025-08-16 01:45:14 +0300

    feat: create sorting

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -5,10 +5,12 @@ import { getAllContacts,
          deleteContact,
          updateContact } from '../services/contacts.js';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js'
 
 export const getContactsController = async(req, res) => {
-    const { page, perPage } = parsePaginationParams(req.query);    
-    const contacts = await getAllContacts({ page, perPage });
+    const { page, perPage } = parsePaginationParams(req.query);  
+    const { sortBy, sortOrder }  = parseSortParams(req.query);  
+    const contacts = await getAllContacts({ page, perPage, sortBy, sortOrder });
         res.status(200).json({
           status: 200,
           message: 'Successfully found contacts!',
