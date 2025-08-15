eab91e202659addaadd2267109f58488853bc2f1
ToriRuban <toriruban@icloud.com>
2025-08-16 00:09:20 +0300

    feat: create pagination

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -4,9 +4,11 @@ import { getAllContacts,
          createNewContact, 
          deleteContact,
          updateContact } from '../services/contacts.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
 
 export const getContactsController = async(req, res) => {
-        const contacts = await getAllContacts();
+    const { page, perPage } = parsePaginationParams(req.query);    
+    const contacts = await getAllContacts({ page, perPage });
         res.status(200).json({
           status: 200,
           message: 'Successfully found contacts!',
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,18 @@
 import { ContactsCollection } from '../db/models/contacts.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 
-export const getAllContacts = async () => {
-    const contacts = await ContactsCollection.find();
-    return contacts;
+export const getAllContacts = async ({ page, perPage }) => {
+    const limit = perPage;
+    const skip = (page - 1) * perPage;
+    const contactsQuery =  ContactsCollection.find();
+    const contactsCount = await ContactsCollection.countDocuments();
+    const contacts = await contactsQuery.skip(skip).limit(limit);
+    const paginationData = calculatePaginationData(contactsCount, perPage, page);
+   
+    return {
+        data: contacts,
+        ...paginationData
+    };
 };
 export const getContactById = async (contactId) => {
     const contact = await ContactsCollection.findById(contactId);
@@ -16,7 +26,7 @@ export const deleteContact = async(contactId) => {
     const contact = await ContactsCollection.findOneAndDelete({
         _id: contactId
     });
-    return contact 
+    return contact;
 };
 export const updateContact = async(contactId, payload) => {
     const result = await ContactsCollection.findOneAndUpdate(
