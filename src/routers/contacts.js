6ca0775829708d206a44653473e91ceef88a7d09
ToriRuban <toriruban@icloud.com>
2025-08-15 17:41:42 +0300

    feat: middleware validation for validId and controllers

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -6,11 +6,26 @@ import { getContactsController,
          patchContactController
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { createContactSchema, updateContactSchema } from '../validation/contactsSchema.js'
+import { isValidId } from '../middlewares/isValidId.js'
 
 const router = Router();
     router.get('/',ctrlWrapper(getContactsController));  
-    router.get('/:contactId', ctrlWrapper(getContactsByIdController));
-    router.post('/', ctrlWrapper(createNewContactController));
-    router.delete('/:contactId', ctrlWrapper(deleteContactController));
-    router.patch('/:contactId', ctrlWrapper(patchContactController));
+    router.get('/:contactId', 
+                isValidId,
+                ctrlWrapper(getContactsByIdController));
+
+    router.post('/', 
+                validateBody(createContactSchema),
+                ctrlWrapper(createNewContactController));
+
+    router.patch('/:contactId',
+                isValidId, 
+                validateBody(updateContactSchema),
+                ctrlWrapper(patchContactController));
+
+    router.delete('/:contactId', 
+                isValidId,
+                ctrlWrapper(deleteContactController));
 export default router;
