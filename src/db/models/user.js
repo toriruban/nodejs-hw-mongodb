5a77a0a1f448598d55017f92be847597816f9975
ToriRuban <toriruban@icloud.com>
2025-08-27 16:21:52 +0300

    feat: create route POST /auth/register to register new user

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -9,4 +9,10 @@ const usersSchema = new Schema (
     { timestamps: true, versionKey: false }, 
 );
 
+usersSchema.methods.toJSON = function () {
+    const obj = this.toObject();
+    delete onj.password;
+    return obj;
+};
+
 export const User = model('users',usersSchema);
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,14 @@
+import bcrypt from 'bcrypt';
+import createHttpError from 'http-errors';
 import { User } from '../db/models/user.js';
 
 export const registerUser = async(payload) => {
-    return await User.create(payload);
+    const user = await User.findOne({ email: payload.email });
+    if(user) throw createHttpError(409, 'Email in use');
+        
+    const encryptedPassword = await bcrypt.hash(payload.password, 10); 
+    return await User.create({
+        ...payload,
+        password: encryptedPassword,
+    });
 };
