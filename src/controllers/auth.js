9cc3189c35e5fd9025a0f2e50d52bec6369cf4b1
ToriRuban <toriruban@icloud.com>
2025-09-02 14:51:11 +0200

    feat: add route refresh to update session based on refresh token

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,4 +1,15 @@
-import { registerUser, loginUser, logoutUser } from '../services/auth.js';
+import { registerUser, loginUser, logoutUser, refreshSession } from '../services/auth.js';
+
+const setupSession = (session, res) => {
+    res.cookie('refreshToken', session.refreshToken, {
+        expires: session.refreshTokenValidUntil,
+        httpOnly: true,
+    });
+    res.cookie('sessionId', session._id, {
+        expires: session.refreshTokenValidUntil,
+        httpOnly: true,
+    });
+}
 
 export const registerUserController = async(req, res) => {
     const user = await registerUser(req.body);
@@ -12,15 +23,7 @@ export const registerUserController = async(req, res) => {
 
 export const loginUserController = async(req, res) => {
     const session = await loginUser(req.body);
-
-    res.cookie('refreshToken', session.refreshToken, {
-        expires: session.refreshTokenValidUntil,
-        httpOnly: true,
-    });
-    res.cookie('sessionId', session._id, {
-        expires: session.refreshTokenValidUntil,
-        httpOnly: true,
-    });
+    setupSession(session, res);
 
     res.status(200).json({
         status: 200,
@@ -37,4 +40,17 @@ export const logoutUserController = async(req, res) => {
     res.clearCookie('refreshToken');
     res.clearCookie('sessionId');
     res.status(204).send();
-}
+};
+
+export const refreshTokenController = async(req, res) => {
+   const session = await refreshSession(req.cookies.sessionId, req.cookies.refreshToken);
+   setupSession(session, res);
+
+   res.status(200).json({
+        status: 200,
+        message: 'Successfully refreshed a session!',
+        data: {
+            accessToken: session.accessToken,
+        },
+});
+};
